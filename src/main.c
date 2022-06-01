8305eac2b3e12e59f27afd65dbce1011758acd1b
Martin Ribelotta <martinribelotta@gmail.com>
2022-06-01 17:44:15 -0300

    Improve donut performance

--- src/main.c
+++ src/main.c
@@ -93,33 +93,50 @@ int led(void)
 
 MSH_CMD_EXPORT(led,  led sample by using I/O drivers);
 
+// Code from: https://gist.github.com/gszauer/5718433
+#define MAX_CIRCLE_ANGLE      512
+#define HALF_MAX_CIRCLE_ANGLE (MAX_CIRCLE_ANGLE/2)
+#define QUARTER_MAX_CIRCLE_ANGLE (MAX_CIRCLE_ANGLE/4)
+#define MASK_MAX_CIRCLE_ANGLE (MAX_CIRCLE_ANGLE - 1)
+#define PI 3.14159265358979323846f
 
-static float tsin[2048];
-static float tcos[2048];
+static float fast_cossin_table[MAX_CIRCLE_ANGLE];           // Declare table of fast cosinus and sinus
 
-static float precalc_sin(float v)
+// Copied from NVidia web site
+static inline void FloatToInt(int *int_pointer, float f) 
 {
-    int idx = v / (2*M_PI) * 2048;
-    int i = idx % 2048;
-    return tsin[idx];
+  *int_pointer = f;
 }
 
-static float precalc_cos(float v)
+static inline float fastcos(float n)
 {
-    int idx = v / (2*M_PI) * 2048;
-    int i = idx % 2048;
-    return tcos[i];
+   float f = n * HALF_MAX_CIRCLE_ANGLE / PI;
+   int i;
+   FloatToInt(&i, f);
+   if (i < 0)
+      return fast_cossin_table[((-i) + QUARTER_MAX_CIRCLE_ANGLE)&MASK_MAX_CIRCLE_ANGLE];
+   else
+      return fast_cossin_table[(i + QUARTER_MAX_CIRCLE_ANGLE)&MASK_MAX_CIRCLE_ANGLE];
 }
 
-void precalc()
+static inline float fastsin(float n)
 {
-    for (int i=0; i<2048; i++) {
-        tsin[i] = sin(2 * i * M_PI / 2048);
-        tcos[i] = cos(2 * i * M_PI / 2048);
-    }
+   float f = n * HALF_MAX_CIRCLE_ANGLE / PI;
+   int i;
+   FloatToInt(&i, f);
+   if (i < 0)
+      return fast_cossin_table[(-((-i)&MASK_MAX_CIRCLE_ANGLE)) + MAX_CIRCLE_ANGLE];
+   else
+      return fast_cossin_table[i&MASK_MAX_CIRCLE_ANGLE];
 }
 
+static void fastsincos_build()
+{
+    for (int i = 0 ; i < MAX_CIRCLE_ANGLE ; i++)
+      fast_cossin_table[i] = (float)sin((double)i * PI / HALF_MAX_CIRCLE_ANGLE);
+}
 
+__attribute__((optimize(3)))
 int donut(void)
 {
     int k;
@@ -127,22 +144,22 @@ int donut(void)
     static float z[1760];
     static char b[1760];
     printf("\x1b[2J"); 
-    precalc();
+    fastsincos_build();
     for(; ; ) {
         memset(b,32,1760);
         memset(z,0,7040);
         for(j=0; 6.28f>j; j+=0.07f) {
             for(i=0; 6.28f >i; i+=0.02f) {
-                float sini=precalc_sin(i),
-                      cosj=precalc_cos(j),
-                      sinA=precalc_sin(A),
-                      sinj=precalc_sin(j),
-                      cosA=precalc_cos(A),
+                float sini=fastsin(i),
+                      cosj=fastcos(j),
+                      sinA=fastsin(A),
+                      sinj=fastsin(j),
+                      cosA=fastcos(A),
                       cosj2=cosj+2,
                       mess=1.0f/(sini*cosj2*sinA+sinj*cosA+5),
-                      cosi=precalc_cos(i),
-                      cosB=precalc_cos(B),
-                      sinB=precalc_sin(B),
+                      cosi=fastcos(i),
+                      cosB=fastcos(B),
+                      sinB=fastsin(B),
                       t=sini*cosj2*cosA-sinj* sinA;
                 int x=40+30*mess*(cosi*cosj2*cosB-t*sinB),
                     y= 12+15*mess*(cosi*cosj2*sinB +t*cosB),
